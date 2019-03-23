a97d48591253b3f720f1822e79b96dc34df05871
Deepak <deepak.rockstar1231@gmail.com>
2019-03-23 17:34:18 +0530

    done exercise 4.2

--- BalaguruSwamySolutions/Chapter4.cpp
+++ BalaguruSwamySolutions/Chapter4.cpp
@@ -7,7 +7,6 @@
 #include <time.h>
 
 using namespace std;
-
 vector<int> newVector;
 void readTheMatrix(int m,int n) {
 	int giveTheLead;
@@ -27,10 +26,42 @@ int main() {
 	cout << "Enter the size of matrix " << endl;
 	cin >> m >> n;
 	readTheMatrix(m, n);
-	for (auto i = newVector.begin(); i != newVector.end(); ++i) {
-		cout << *i << " ";
-	}
+	int startOfVector = 0;
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < n; k++) {
+				cout << newVector.at(startOfVector) << " ";
+				startOfVector++;
+			}
+			cout << endl;
+		}
 	//cout << "the mattter is conveyed" << endl;
 	return 0;
 }
+//***************************************************************************************************************
+//Exercise 4.1
+//vector<int> newVector;
+//void readTheMatrix(int m,int n) {
+//	int giveTheLead;
+//	cout << "Enter the numbers you wanted to push into the matrix" << endl;
+//	for (int i = 0; i < m; i++) {
+//		cout << "Now you are in row " << i << endl;
+//		for (int j = 0; j < n; j++) {
+//			cout << "Enter the numbers you wanted in row " << i << "column " << j << endl;
+//			cin >> giveTheLead;
+//			newVector.push_back(giveTheLead);
+//		}
+//	}
+//}
+//
+//int main() {
+//	int m, n;
+//	cout << "Enter the size of matrix " << endl;
+//	cin >> m >> n;
+//	readTheMatrix(m, n);
+//	for (auto i = newVector.begin(); i != newVector.end(); ++i) {
+//		cout << *i << " ";
+//	}
+//	//cout << "the mattter is conveyed" << endl;
+//	return 0;
+//}
 
